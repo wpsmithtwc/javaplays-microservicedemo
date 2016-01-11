356ce032b2c3e432b532e3c0aef500e43823fc95
danielcho <danielcho@us.ibm.com>
2016-01-11 02:28:52 -0500

    api update

--- api-service-choreographer/src/main/java/com/ibm/ecoddemo/microservice/servicechoreographer/MobileServiceChoreographer.java
+++ api-service-choreographer/src/main/java/com/ibm/ecoddemo/microservice/servicechoreographer/MobileServiceChoreographer.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("mobile")
+@RequestMapping("/mobile")
 public class MobileServiceChoreographer {
 	
 	@RequestMapping("/findby/number/{accountNumber}")
