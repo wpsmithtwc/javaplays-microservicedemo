0d6f5fa0c00e5a27d83b9fdd27aead2e8b01ce02
danielcho <danielcho@us.ibm.com>
2016-01-11 20:59:45 -0500

    api update

---
+++ api-service-choreographer/src/main/java/com/ibm/ecoddemo/microservice/servicechoreographer/CustomerAccountDTO.java
@@ -0,0 +1,123 @@
+package com.ibm.ecoddemo.microservice.servicechoreographer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+/**
+ * 
+ * @author danielcho
+ *
+ */
+@JsonRootName("CustomerAccount")
+public class CustomerAccountDTO {
+
+	private Long id;
+	private String customerNo;
+	private String name;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String phone;
+	private String email;
+	private List<AccountDTO> accounts;
+
+	public CustomerAccountDTO() {
+		/* setup default values */
+		id = Long.valueOf(0);
+		customerNo = "";
+		name = "";
+		address = "";
+		city = "";
+		state = "";
+		zipCode = "";
+		phone = "";
+		email = "";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCustomerNo() {
+		return customerNo;
+	}
+
+	public void setCustomerNo(String customerNo) {
+		this.customerNo = customerNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void addAccounts(AccountDTO account) {
+		if (accounts == null) {
+			accounts = new ArrayList<AccountDTO>(0);
+		}
+		accounts.add(account);
+	}
+
+	public List<AccountDTO> getAccounts() {
+		return accounts;
+	}
+
+}
--- api-service-choreographer/src/main/java/com/ibm/ecoddemo/microservice/servicechoreographer/MobileServiceChoreographer.java
+++ api-service-choreographer/src/main/java/com/ibm/ecoddemo/microservice/servicechoreographer/MobileServiceChoreographer.java
@@ -1,5 +1,11 @@
 package com.ibm.ecoddemo.microservice.servicechoreographer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mobile")
 public class MobileServiceChoreographer {
 	
+	private @Resource(name="accountsServiceClient") AccountsServiceClient accountsServiceClient;
+	private @Resource(name="customersServiceClient") CustomersServiceClient customersServiceClient;
+	
 	@RequestMapping("/accounts/findby/number/{accountNumber}")
-	public String findByAccountNumber(@PathVariable("accountNumber") String accountNumber) {
-		return "findByAccountNumber reachable";
+	public CustomerAccountDTO findByAccountNumber(@PathVariable("accountNumber") String accountNumber) {
+		/* a request from mobile device fetches both customer and account information from backend processes. */
+		CustomerAccountDTO customerAccount = new CustomerAccountDTO();
+		AccountDTO account = accountsServiceClient.searchByNumber(accountNumber);
+		customerAccount.addAccounts(account);
+		CustomerDTO customer = customersServiceClient.searchByNumber(account.getCustomerNo());
+		BeanUtils.copyProperties(customer, customerAccount);
+		return customerAccount;
 	}
 
 	@RequestMapping("/accounts/findby/name/{name}")
-	public String findByAccountOwnerName(@PathVariable("name") String partialName) {
-		return "findByAccountOwnerName reachable";
+	public List<CustomerAccountDTO> findByAccountOwnerName(@PathVariable("name") String partialName) {
+		List<CustomerAccountDTO> customerAccounts = new ArrayList<CustomerAccountDTO>(0);
+		CustomerAccountDTO customerAccount = null;
+		CustomerDTO customer = null;
+		for (AccountDTO account : accountsServiceClient.searchByOwnerContains(partialName)) {
+			customer = customersServiceClient.searchByNumber(account.getCustomerNo());
+			if (customer != null) {
+				customerAccount = new CustomerAccountDTO();
+				customerAccount.addAccounts(account);
+				BeanUtils.copyProperties(customer, customerAccount);
+			}
+		}
+		
+		
+		return customerAccounts;
 	}
 }
