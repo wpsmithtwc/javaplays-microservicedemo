463847fb93679ae767f420d60bdd73e8db0dcd65
danielcho <danielcho@us.ibm.com>
2016-01-15 12:28:43 -0500

    configuration update

--- configuration-service/src/main/java/com/ibm/ecoddemo/microservice/configuration/ConfigurationService.java
+++ configuration-service/src/main/java/com/ibm/ecoddemo/microservice/configuration/ConfigurationService.java
@@ -2,19 +2,32 @@ package com.ibm.ecoddemo.microservice.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/***
+ * 
+ * @author danielcho
+ * 
+ * PURPOSE
+ * This microervice fetches configurations from a version control management system for other microservices to
+ * load when instantiated.
+ * 
+ * DRIVING FORCE
+ * 1. Versioned configuration management in lieu of data base managed if configuration externalization is application 
+ *    strategy, which is a common practice.
+ * 2. Avoid service outage to update configuration when changes during up time - requires service bus enabled.
+ */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigurationService {
+public class ConfigurationService extends SpringBootServletInitializer {
 
-	/* PURPOSE
-	 * This microservice serves as REST based API to provide external configuration.
-	 * 
-	 * DRIVING FORCE
-	 * 1. Versioned configuration management in distribution system
-	 * 2. Avoid service outage to update configuration.
-	 **/
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigurationService.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationService.class, args);
 	}
