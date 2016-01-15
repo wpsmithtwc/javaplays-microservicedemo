8621e35b02766f71d09a9687d71ec292f2170955
danielcho <danielcho@us.ibm.com>
2016-01-15 14:16:38 -0500

    configuration update

--- discovery-service/src/main/java/com/ibm/ecoddemo/microservice/discovery/DiscoveryService.java
+++ discovery-service/src/main/java/com/ibm/ecoddemo/microservice/discovery/DiscoveryService.java
@@ -2,12 +2,28 @@ package com.ibm.ecoddemo.microservice.discovery;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/***
+ * 
+ * @author danielcho
+ * 
+ * PURPOSE
+ * 
+ * DRIVING FORCE
+ * 
+ */
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryService {
+public class DiscoveryService extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DiscoveryService.class);
+    }
+	
 	/* Discovery service:  
 	 * Using logical name (service id instead of actual end point,
 	 * multiple processes must be able to find each other to collaborate during runtime. */
